3ba15aae343c842b7605dbd43335da63b3ed079e
dikshaverma1502 <94672326+dikshaverma1502@users.noreply.github.com>
2022-07-12 15:59:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+  "01d": "/icons/sunny.svg",
+  "01n": "/icons/night.svg",
+  "02d": "/icons/day.svg",
+  "02n": "/icons/cloudy-night.svg",
+  "03d": "/icons/cloudy.svg",
+  "03n": "/icons/cloudy.svg",
+  "04d": "/icons/perfect-day.svg",
+  "04n": "/icons/cloudy-night.svg",
+  "09d": "/icons/rain.svg",
+  "09n": "/icons/rain-night.svg",
+  "10d": "/icons/rain.svg",
+  "10n": "/icons/rain-night.svg",
+  "11d": "/icons/storm.svg",
+  "11n": "/icons/storm.svg",
+};
+
+const Container =styled.div`
+display: flex;
+flex-direction: column;
+margin: auto;
+align-items: center;
+box-shadow : 0 3px 6px 0 #555;
+padding : 20px 10px;
+border-radius: 4px;
+width: 380px;
+background: white;
+`;
+
+
+const AppLabel = styled.span`
+color: black;
+font-size: 18px;
+font-weight: bold;
+font-family: 'Poppins', sans-serif;
+`
+
+
+function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+    );
+    updateWeather(response.data);
+  };
+  return (
+    <Container>
+      <AppLabel>React Weather App</AppLabel>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
